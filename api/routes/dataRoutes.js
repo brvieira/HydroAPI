5e04fc179ea5bd05b19a05fe241090b9959dfdb2
Bruno Vieira Rosa <bruno.silvavieira@gmail.com>
2018-07-17 15:11:57 -0300

    Correção rota /dados

--- api/routes/dataRoutes.js
+++ api/routes/dataRoutes.js
@@ -3,8 +3,7 @@
 module.exports = function(app){
     var dados = require('../controllers/dataController');
 
-    app.route('/dados')
-        .get(dados.listAll)
-        .post(dados.save);
+    app.route('/dados').get(dados.listAll);
+    app.route('/dados').post(dados.save);
 
 };
