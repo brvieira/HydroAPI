e2f7e7dfb1c1b3a922b1a6328ce92bbfb834d071
Bruno Vieira <Bruno.Vieira.Rosa@ibm.com>
2019-04-26 17:23:25 -0300

    Processamento de dados

--- api/controllers/dadosController.js
+++ api/controllers/dadosController.js
@@ -10,7 +10,31 @@ module.exports = () => {
     }
 
     const listAll = async () => {
-        return await colecaoDados.select({});
+        let result = [];
+        try {
+            let data = await colecaoDados.select({});
+            
+            for (let item of data) {
+                for (let metric in item) {
+                    if (metric != "_id" && metric != "token" && metric != "data") {
+                        if (!result.find(x => x.name == metric)) {
+                            result.push({ name: metric, data: [] });
+                        }
+
+                        result
+                            .find(x => x.name == metric)
+                            .data.push({
+                                x: item.data,
+                                y: item[metric]
+                            });
+                    }
+                }
+            }
+        } catch (error) {
+            console.error(error);
+        } finally {
+            return result;
+        }
     }
 
     const listByDateRange = async (dataInicial, dataFinal) => {
--- api/controllers/usuariosController.js
+++ api/controllers/usuariosController.js
@@ -1,6 +1,21 @@
 'use strict'
-const colecaoUsuarios = require('./api/utils/mongoConnector')('usuarios');
+const colecaoUsuarios = require('../utils/mongoConnector')('usuarios');
+const bcrypt = require('bcrypt');
 
 module.exports = () => {
-    
+    const newUser = async (data) => {
+        return await colecaoUsuarios.insert(data);
+    }
+
+    const login = async (data) => {
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+    //TODO - Implementar função para inserção de novos nós sensores
+    const addNode = async (data) => {
+
+    }
 }
